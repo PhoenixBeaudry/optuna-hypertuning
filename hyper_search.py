660ed4be379a22a448f33ca5a46d07839e9e332c
Jeff <jeffc.bourassa@gmail.com>
2024-01-01 14:35:06 -0800

    remove code duplication by importing from train.py

--- hyper_search.py
+++ hyper_search.py
@@ -16,52 +16,8 @@ import pywt
 from dotenv import load_dotenv
 import os
 import tensorflow_addons as tfa
-from train import upper_shadow, lower_shadow, add_daily_open_feature, add_technical_indicators, scale_data, perform_wavelet_transform, create_dataset, calculate_weighted_rmse, decaying_rmse_loss
+from train import scale_data, perform_wavelet_transform, create_dataset, calculate_weighted_rmse, decaying_rmse_loss, get_data
 
-# Load the .env file
-load_dotenv()
-
-#Load the data from the pickle file
-with open('data/2y_data.pickle', 'rb') as file:
-    data_structure = pickle.load(file)
-
-data_structure = [data_structure[0],
-data_structure[1],
-data_structure[2],
-data_structure[3],
-data_structure[4]]
-
-data_array = np.array(data_structure).T
-
-#Assuming the structure is [timestamps, close, high, low, volume]
-timestamps = pd.to_datetime(data_array[:, 0], unit='ms')
-close_prices = data_array[:, 1]
-high_prices = data_array[:, 2]
-low_prices = data_array[:, 3]
-volumes = data_array[:, 4]
-
-# Combine all features into a DataFrame
-data_df = pd.DataFrame({
-    'timestamps': timestamps,
-    'close': close_prices,
-    'high': high_prices,
-    'low': low_prices,
-    'volume': volumes
-})
-
-
-# Create more advanced technical indicators
-df = add_technical_indicators(data_df)
-# remove all NaN values
-df.dropna(inplace=True)
-df.drop('timestamps', axis=1, inplace=True)
-df.drop('date', axis=1, inplace=True)
-
-data  = df.values
-
-scaler = MinMaxScaler(feature_range=(0, 1))
-
-num_features = data.shape[1]
 
 # Optuna objective
 def objective(trial):
@@ -122,9 +78,9 @@ def objective(trial):
     
     # Data prep
     if wavelet_transform == "True": 
-        X, y = create_dataset(scale_data(perform_wavelet_transform(data, wavelet=wavelet_type, level=decomposition_level)), num_previous_intervals, 100)
+        X, y = create_dataset(scale_data(perform_wavelet_transform(data, wavelet=wavelet_type, level=decomposition_level), scaler), num_previous_intervals, 100)
     else: 
-        X, y = create_dataset(scale_data(data), num_previous_intervals, 100)
+        X, y = create_dataset(scale_data(data, scaler), num_previous_intervals, 100)
     
     # Split into train and test set
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42, shuffle=False)
@@ -164,10 +120,19 @@ def objective(trial):
     return rmse
 
 
-database_url = os.environ.get('DATABASE_URL')
+if __name__ == "__main__":
 
-#Create Study
-study = optuna.create_study(direction='minimize', study_name="regularization", load_if_exists=True, storage=database_url)
+    # Load the .env file
+    load_dotenv()
 
-# Do the study
-study.optimize(objective)  # Adjust the number of trials
+    df, data, num_features = get_data('data', '2y_data.pickle')
+
+    scaler = MinMaxScaler(feature_range=(0, 1))
+
+    database_url = os.environ.get('DATABASE_URL')
+
+    #Create Study
+    study = optuna.create_study(direction='minimize', study_name="regularization", load_if_exists=True, storage=database_url)
+
+    # Do the study
+    study.optimize(objective)  # Adjust the number of trials

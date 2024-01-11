1e44923cdd54c3d269569edbeb5c3f0a31d88dad
Phoenix Beaudry <phoenixdbeaudry@gmail.com>
2024-01-10 16:17:53 -0800

    training file for new trials

--- train.py
+++ train.py
@@ -26,29 +26,86 @@ if __name__ == "__main__":
     data = training_data.values
     num_features = data.shape[1]
 
+    ############# SEARCH PARAMS #############
     #Layers
-    hidden_units = 400
-    dropout_rate = 0.2
-    num_previous_intervals = 50
+    bidirectional = False
+    num_layers = 1 
+    layer_multiplier = 1 
+    hidden_units = 949
+    dropout_rate = 0.16300714101033043
+    num_previous_intervals = 133
+    
+    
+    # Elastic Net Regularization hyperparameters
+    elastic_net = False
+    l1_reg = 0 
+    l2_reg = 0
+
 
     # Optimizer
-    learning_rate = 1e-3
-    sync_period = 3
-    slow_step_size = 0.4
+    optimizer_type = 'ranger'#trial.suggest_categorical('optimizer_type', ['adam', 'ranger'])
+    learning_rate = 0.01312969026939553
+    sync_period = 9
+    slow_step_size = 0.7496038687048765
+    total_steps = 14103
+    warmup_proportion = 0.1785902006326781
+    min_lr =  5.219265219794206e-06
 
+    #Callbacks
+    lr_reduction_factor = 0.2979832620972566
     #Training
-    batch_size = 256
+    batch_size = 768
 
-    # Optimizer
-    adam = tf.keras.optimizers.legacy.Adam(learning_rate=learning_rate)
-    optimizer = tfa.optimizers.Lookahead(adam, sync_period=sync_period, slow_step_size=slow_step_size)
+    
 
     # Create a model with the current trial's hyperparameters
     model = Sequential()
-    model.add((LSTM(hidden_units, return_sequences=False, input_shape=(num_previous_intervals, num_features))))
-    model.add(Dropout(dropout_rate))
-    model.add(Dense(100, activation='linear'))
-    model.compile(optimizer=adam, loss=decaying_rmse_loss)
+    for i in range(num_layers):
+        if i == 0:
+            if elastic_net:
+                if bidirectional:
+                    model.add(Bidirectional(LSTM(hidden_units, return_sequences=num_layers > 1,
+                            input_shape=(num_previous_intervals, num_features),
+                            kernel_regularizer=l1_l2(l1=l1_reg, l2=l2_reg)))) # Apply Elastic Net regularization
+                else:
+                    model.add((LSTM(hidden_units, return_sequences=num_layers > 1,
+                            input_shape=(num_previous_intervals, num_features),
+                            kernel_regularizer=l1_l2(l1=l1_reg, l2=l2_reg)))) # Apply Elastic Net regularization
+            else:
+                if bidirectional:
+                    model.add(Bidirectional(LSTM(hidden_units, return_sequences=num_layers > 1,
+                            input_shape=(num_previous_intervals, num_features))))
+                else:
+                    model.add((LSTM(hidden_units, return_sequences=num_layers > 1,
+                            input_shape=(num_previous_intervals, num_features))))
+        else:
+            if elastic_net:
+                if bidirectional:
+                    model.add(Bidirectional(LSTM(int(hidden_units*layer_multiplier*i), return_sequences=i < num_layers - 1,
+                            kernel_regularizer=l1_l2(l1=l1_reg, l2=l2_reg)))) # Apply Elastic Net regularization
+                else:
+                    model.add((LSTM(int(hidden_units*layer_multiplier*i), return_sequences=i < num_layers - 1,
+                            kernel_regularizer=l1_l2(l1=l1_reg, l2=l2_reg)))) # Apply Elastic Net regularization
+            else:
+                if bidirectional:
+                    model.add(Bidirectional(LSTM(int(hidden_units*layer_multiplier*i), return_sequences=i < num_layers - 1)))
+                else:
+                    model.add(LSTM(int(hidden_units*layer_multiplier*i), return_sequences=i < num_layers - 1))
+        model.add(Dropout(dropout_rate))
+    if elastic_net:
+        model.add(Dense(100, kernel_regularizer=l1_l2(l1=l1_reg, l2=l2_reg))) # Apply Elastic Net
+    else:
+        model.add(Dense(100))
+
+    if optimizer_type == 'ranger':
+        radam = tfa.optimizers.RectifiedAdam(learning_rate=learning_rate, total_steps=total_steps, warmup_proportion=warmup_proportion, min_lr=min_lr)
+        optimizer = tfa.optimizers.Lookahead(radam, sync_period=sync_period, slow_step_size=slow_step_size)
+    elif optimizer_type == 'adam':
+        optimizer = tf.keras.optimizers.Adam(learning_rate=learning_rate)
+    else:
+        optimizer = tf.keras.optimizers.Adam(learning_rate=learning_rate)
+    
+    model.compile(optimizer=optimizer, loss=decaying_rmse_loss)
 
     df_train, df_test = train_test_split(data, test_size=0.05, shuffle=False)
 
@@ -65,15 +122,15 @@ if __name__ == "__main__":
     X_test, y_test = create_dataset(df_test_scaled, num_previous_intervals)
 
     # Early stopping callback
-    early_stopping = EarlyStopping(monitor='val_loss', patience=15, restore_best_weights=True)
-    model_checkpoint = ModelCheckpoint('trained_models/formless-v2_1.h5', monitor='val_loss', save_best_only=True)
-    reduce_lr = ReduceLROnPlateau(monitor='val_loss', factor=0.2, patience=7, min_lr=1e-7, verbose=1)
+    early_stopping = EarlyStopping(monitor='val_loss', patience=7, restore_best_weights=True)
+    model_checkpoint = ModelCheckpoint('trained_models/formless-v2_2.h5', monitor='val_loss', save_best_only=True)
+    reduce_lr = ReduceLROnPlateau(monitor='val_loss', factor=lr_reduction_factor, patience=3, min_lr=min_lr, verbose=1)
 
     # Train the model
     model.fit(X_train, y_train, epochs=100, batch_size=batch_size, validation_split=0.2, verbose=1, callbacks=[early_stopping, model_checkpoint, reduce_lr])
 
     # Save the scaler
-    with open('trained_models/formless-v2_1_scaler.pkl', 'wb') as file:
+    with open('trained_models/formless-v2_2_scaler.pkl', 'wb') as file:
         pickle.dump(scaler, file)
 
     # Evaluate the model
